8b6e847b17c91704418b16f8f71ffdf67ff03ca0
Antoine Menini <antoine.menini@cerbair.com>
2020-04-06 11:32:33 +0200

    Change seed.

--- orthos.cpp
+++ orthos.cpp
@@ -1,12 +1,13 @@
 #include<iostream>
 #include<cstdlib>
-#include<ctime>
+#include <chrono>
 #include "orthos.hpp"
  
  
 int orthos::getRand(void)
 {
-    srand(time(NULL));
+	int seed = (int)(std::chrono::system_clock::now().time_since_epoch() / std::chrono::milliseconds(1));
+    srand(seed);
     return rand();
 }
 
